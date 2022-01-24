7de68ae2ba1ba839587e942c5e7a75636ae2da9f
nazarovshunqor <nazarovshunqor@gmail.com>
2022-01-24 09:44:53 +0500

    first lesson

---
+++ script.js
@@ -0,0 +1,91 @@
+"use strict";
+
+// let number = 3;
+
+// console.log(number);
+
+
+// number = 5;
+// console.log(number);
+
+// const pi = 3.14;
+
+// const obj = {
+//   a: 10,
+// }
+// obj.a = 20
+
+// const number = 8; // data-type = number
+// const name = 'Shunqorbek'; // data-type = string
+// const bool = true; // data-type = boolean
+
+
+
+// console.log(a); // data-type = null topilmadi
+
+// let lastName;
+// console.log(lastName); //data-type = undefined bizda karopka bor lekin uni ichida hech narsa yo'q
+
+// console.log(-4 / 0);
+
+
+// console.log("samar" * 10); // NaN Not a Number = Son emas
+
+// obyektlar {} ichida yoziladi masalan
+// const person = {
+
+// }
+// obyektni ichiga = qo'yilmaydi : qo'yiladi
+
+// const person = {
+//   firstName: "Shunqorbek",
+//   lstName: "Nazarov",
+//   age: 14,
+//   job: "Full-Stack JS developer",
+//   isMaried: false,
+// }
+
+// console.log(person.age);
+
+
+// bizda yana massivlar ham bor const arr = ["red", 10, {}, [], true],,
+//  massivlarni ichida ham raqam ham text ham obyektlarni yozsa bo'ladi
+
+
+
+// const arr = [age = 15, lastName = "shunqor"] // console.log(arr)
+
+
+// console.log("hello world")
+// alert("hello world")
+
+
+// const userAge = confirm('Siz 18 yoshdamisiz');
+// console.log(userAge);
+
+
+
+
+// const userName = prompt("Ismiz nma", 'user name');
+// console.log(userName);
+
+
+// const answers = [];
+// answers[0] = prompt("Ismingiz nma");
+// answers[1] = prompt("Familyangiz nma");
+// answers[2] = prompt("Yoshingiz nechida");
+
+// console.log(answers)
+
+// const firstName = prompt("Ismingiz nma");
+// console.log(typeof firstName);
+
+
+// typeof bu data-type turini aniqlidi
+// agar promptni oldiga + qo'ysa u ""  ichidagi sonni numberga aylantiradi 
+
+// const firstName = 'Shunqorbek'
+// console.log(`Mani ismim ${firstName}`)
+
+
+// `` bunday elgi bo'ganda biz textni ichiga o'zgaruvchanni ${} ichidan yozib ketsak bo'ladi
